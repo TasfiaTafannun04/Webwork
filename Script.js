2fb99e7094a1a0b027c2084c9765386948ad74e2
TasfiaTafannun04 <tafannuntasfia04@gmail.com>
2024-12-25 23:21:42 +0600

    booksdiplaywasapain

--- Script.js
+++ Script.js
@@ -2,7 +2,7 @@ document.addEventListener('scroll', () => {
     const g1strow = document.querySelector('#gal1strow');
     const ga1pos = g1strow.getBoundingClientRect().top;
     const screenheight = window.innerHeight;
-    if(ga1pos < screenheight){
+    if (ga1pos < screenheight) {
         g1strow.classList.add('animate');
     }
 });
@@ -36,7 +36,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 document.addEventListener('scroll', () => {
     const book1 = document.querySelector('#book1');
-    const book1pos = book1.getBoundingClientRect().top;
+    const parentb = document.querySelector('#displaybook');
+    const book1pos = parentb.getBoundingClientRect().top;
     const screenHeight = window.innerHeight;
     if (book1pos < screenHeight) {
         book1.classList.add('animate');
@@ -45,7 +46,8 @@ document.addEventListener('scroll', () => {
 
 document.addEventListener('scroll', () => {
     const book2 = document.querySelector('#book2');
-    const book2pos = book2.getBoundingClientRect().top;
+    const parentb = document.querySelector('#displaybook');
+    const book2pos = parentb.getBoundingClientRect().top;
     const screenHeight = window.innerHeight;
     if (book2pos < screenHeight) {
         book2.classList.add('animate');
@@ -54,7 +56,8 @@ document.addEventListener('scroll', () => {
 
 document.addEventListener('scroll', () => {
     const book3 = document.querySelector('#book3');
-    const book3pos = book3.getBoundingClientRect().top;
+    const parentb = document.querySelector('#displaybook');
+    const book3pos = parentb.getBoundingClientRect().top;
     const screenHeight = window.innerHeight;
     if (book3pos < screenHeight) {
         book3.classList.add('animate');
